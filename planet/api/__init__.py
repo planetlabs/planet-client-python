8e09f8899c589666b60b5e1db7ead9cb13c204b0
Chris Holmes <chomie@gmail.com>
2015-06-17 17:22:02 -0700

    apache license updates
    Conflicts:
    	planet/scripts/__init__.py

--- planet/api/__init__.py
+++ planet/api/__init__.py
@@ -1,3 +1,16 @@
+# Copyright 2015 Planet Labs, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 from datetime import datetime
 import logging
@@ -90,7 +103,7 @@ def strf_timestamp(when):
 class Response(object):
 
     def __init__(self, response):
-        
+
         self.response = response
         self.size = int(self.response.headers.get('content-length', -1))
         self.name = _get_filename(self.response)
@@ -213,7 +226,7 @@ class Client(object):
     def get_scene_metadata(self, scene_id, scene_type='ortho'):
         """
         Get metadata for a given scene.
-        
+
         .. todo:: Generalize to accept multiple scene ids.
         """
         return JSON(self._get('scenes/%s/%s' % (scene_type, scene_id)))
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,3 +1,17 @@
+# Copyright 2015 Planet Labs, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 '''
 Command line specific tests - the client should be completely mocked and the
 focus should be on asserting any CLI logic prior to client method invocation
@@ -47,75 +61,75 @@ def test_exception_translation():
 
 
 def test_version_flag():
-    
+
     results = runner.invoke(scripts.cli, ['--version'])
     assert results.output == "%s\n" % planet.__version__
 
 
 def test_list_scene_types():
-    
+
     fixture_path = os.path.join(FIXTURE_DIR, 'scene-types.json')
     with open(fixture_path, 'r') as src:
         expected = src.read()
-    
+
     response = MagicMock(spec=api.JSON)
     response.get_raw.return_value = expected
-    
+
     scripts.client.list_scene_types.return_value = response
-    
+
     result = runner.invoke(scripts.cli, ['list-scene-types'])
-    
+
     assert_success(result, expected)
 
 
 def test_search():
-    
+
     fixture_path = os.path.join(FIXTURE_DIR, 'search.geojson')
     with open(fixture_path, 'r') as src:
         expected = src.read()
-    
+
     response = MagicMock(spec=api.JSON)
     response.get_raw.return_value = expected
-    
+
     scripts.client.get_scenes_list.return_value = response
-    
+
     result = runner.invoke(scripts.cli, ['search'])
-    
+
     assert_success(result, expected)
-    
+
 
 def test_search_by_aoi():
-    
+
     aoi_path = os.path.join(FIXTURE_DIR, 'aoi.geojson')
     with open(aoi_path, 'r') as src:
         aoi = src.read()
-    
+
     fixture_path = os.path.join(FIXTURE_DIR, 'search-by-aoi.geojson')
     with open(fixture_path, 'r') as src:
         expected = src.read()
-    
+
     response = MagicMock(spec=api.JSON)
     response.get_raw.return_value = expected
-    
+
     scripts.client.get_scenes_list.return_value = response
-    
+
     # input kwarg simulates stdin
     result = runner.invoke(scripts.cli, ['search'], input=aoi)
-    
+
     assert_success(result, expected)
-    
+
 
 def test_metadata():
-    
+
     # Read in fixture
     fixture_path = os.path.join(FIXTURE_DIR, '20150615_190229_0905.geojson')
     with open(fixture_path, 'r') as src:
         expected = src.read()
-    
+
     # Construct a response from the fixture
     response = MagicMock(spec=api.JSON)
     response.get_raw.return_value = expected
-    
+
     # Construct the return response for the client method
     scripts.client.get_scene_metadata.return_value = response
 
