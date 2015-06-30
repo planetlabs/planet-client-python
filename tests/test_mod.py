e812643452cec0901ccb330c7196e584d8f8e7ed
Ian Schneider <planablediglet@gmail.com>
2015-06-29 23:20:05 -0600

    flake8 and redundant/unused cleanup, safer asserts

--- tests/test_mod.py
+++ tests/test_mod.py
@@ -27,135 +27,66 @@ client = api.Client()
 client.dispatcher.set_api_key('foobar')
 
 
-# @pytest.fixture(scope="module")
-# def mock_get(path, data, status_code=200):
-#
-#     def outer(func):
-#
-#         @requests_mock.mock()
-#         def inner(m):
-#             m.register_uri('GET', client.base_url + path,
-#                            text=data, status_code=status_code)
-#             func()
-#
-#         return inner
-#
-#     return outer
-
-
-def assert_client_exc(clz, msg, status=None):
-    try:
-        client._get('whatevs')
-    except clz as ex:
-        assert msg == ex.message
-    else:
-        raise AssertionError('expected %s' % clz.__name__)
-
-
 def test_assert_client_execution_success():
-    '''make sure our test works'''
-    
+    'verify simple mock success response'
     with requests_mock.Mocker() as m:
-
         uri = os.path.join(client.base_url, 'whatevs')
         m.get(uri, text='test', status_code=200)
-        
-        # Since the Client object captures the entire response, there's
-        # not much to test against. Just check that an exception is not raised.
-        try:
-            client._get('whatevs')
-            assert True
-        except:
-            assert False
-
-
-def test_assert_client_execution_failure():
-    '''make sure our test works'''
-    
-    with requests_mock.Mocker() as m:
-
-        uri = os.path.join(client.base_url, 'whatevs')
-        m.get(uri, text='test', status_code=404)
-        
-        # Check that an exception is raised
-        try:
-            client._get('whatevs').get_body()
-        except api.APIException as e:
-            assert True
-            return
-        
-        assert False
+        assert 'test' == client._get('whatevs').get_body().get_raw()
 
 
 def test_missing_api_key():
-    
+    '''verify exception raised on missing API key'''
     client = api.Client()
-
-    def assert_missing():
-        try:
-            client._get('whatevs').get_body()
-            assert False
-        except api.InvalidAPIKey as ex:
-            assert str(ex) == 'No API key provided'
-
     # verify no auth headers trigger the exception
     # have to clear the dispatcher in case key is picked up via env
     client.dispatcher.set_api_key(None)
-    assert_missing()
+    try:
+        client._get('whatevs').get_body()
+    except api.InvalidAPIKey as ex:
+        assert str(ex) == 'No API key provided'
+    else:
+        assert False
 
 
 def test_status_code_404():
-    
+    '''Verify 404 handling'''
     with requests_mock.Mocker() as m:
-        
         uri = os.path.join(client.base_url, 'whatevs')
-        m.get(uri, text='not exist', status_code=404)
-        
+        m.get(uri, text='test', status_code=404)
         try:
             client._get('whatevs').get_body()
-        except api.MissingResource as e:
-            
-            # TODO: Check returned string. Currently issues between Python 3 bytes and Python 2 strings
-            assert True
-            return
-        
-        assert False
+        except api.MissingResource, ex:
+            assert ex.message == 'test'
+        else:
+            assert False
 
 
 def test_status_code_other():
-    
+    '''Verify other unexpected HTTP status codes'''
     with requests_mock.Mocker() as m:
-        
         uri = os.path.join(client.base_url, 'whatevs')
+        # yep, this is totally made up
         m.get(uri, text='emergency', status_code=911)
-        
         try:
             client._get('whatevs').get_body()
-        except api.APIException as e:
-            
-            assert True
-            return
-        
-        assert False
+        except api.APIException, ex:
+            assert ex.message == '911: emergency'
+        else:
+            assert False
 
 
 def test_list_all_scene_types():
-
+    '''Verify list_all_scene_types path handling'''
     with requests_mock.Mocker() as m:
-        
         uri = os.path.join(client.base_url, 'scenes')
         m.get(uri, text='oranges', status_code=200)
-        
-        # TODO: Check returned string. Currently issues between Python 3 bytes and Python 2 strings
         assert client.list_scene_types().get_raw() == 'oranges'
 
 
-
 def test_fetch_scene_info_scene_id():
-
+    '''Verify get_scene_metadata path handling'''
     with requests_mock.Mocker() as m:
-        
         uri = os.path.join(client.base_url, 'scenes/ortho/x22')
         m.get(uri, text='bananas', status_code=200)
-        
         client.get_scene_metadata('x22').get_raw() == 'bananas'
