7563ec2b72cd0054f3f1b9911af86670c3bc7d1a
Ian Schneider <planablediglet@gmail.com>
2015-08-25 11:26:01 -0600

    alabaster doc theme adjustments

---
+++ docs/source/alabaster_planet.py
@@ -0,0 +1,13 @@
+pblue = '#008fd5'
+sblue = '#18aae6'
+pgrey = ' #666666'
+
+html_theme_options = {
+    'logo': 'logo.png',
+    'body_text': '#000',
+    'gray_1': pgrey,
+    'link': pblue,
+    'sidebar_search_button': pblue,
+    'sidebar_link': pblue,
+    'github_button': 'false',
+}
