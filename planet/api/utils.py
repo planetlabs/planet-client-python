e62a08cb628848b5266d01962384ef366faf436e
Ian Schneider <planablediglet@gmail.com>
2017-02-08 08:44:21 -0700

    check status ok on all 2xx http status

--- planet/api/utils.py
+++ planet/api/utils.py
@@ -107,7 +107,7 @@ def geometry_from_json(obj):
 def check_status(response):
     '''check the status of the response and if needed raise an APIException'''
     status = response.status_code
-    if status == 200:
+    if status < 300:
         return
     exception = {
         400: exceptions.BadQuery,
