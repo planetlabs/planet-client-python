e45c5d14e8898e164f4e11f27ec3253cc34d197d
Ian Schneider <planablediglet@gmail.com>
2017-03-02 09:58:40 -0700

    add TooManyRequests to api namespace

--- planet/api/__init__.py
+++ planet/api/__init__.py
@@ -14,12 +14,13 @@
 
 from .exceptions import (APIException, BadQuery, InvalidAPIKey)
 from .exceptions import (NoPermission, MissingResource, OverQuota)
-from .exceptions import (ServerError, RequestCancelled)
+from .exceptions import (ServerError, RequestCancelled, TooManyRequests)
 from .client import (ClientV1)
 from .utils import write_to_file
 
 __all__ = [
     ClientV1, APIException, BadQuery, InvalidAPIKey,
     NoPermission, MissingResource, OverQuota, ServerError, RequestCancelled,
+    TooManyRequests,
     write_to_file
 ]
