71dd2076b2d76b06dc37782a82ab25c4e705ad4d
jreiberkyle <jreiberkyle@users.noreply.github.com>
2020-12-02 12:09:01 -0800

    improve documentation, increase test coverage

--- planet/api/models.py
+++ planet/api/models.py
@@ -14,10 +14,11 @@
 """Manage data for requests and responses."""
 
 import datetime
+# import itertools
+# import json
 import logging
 
 from ._fatomic import atomic_open
-# from .exceptions import InvalidAPIKey, RequestCancelled
 from . import exceptions, utils
 
 
@@ -27,8 +28,17 @@ LOGGER = logging.getLogger(__name__)
 
 
 class Request(object):
+    '''Handles a HTTP request for the Planet server'''
     def __init__(self, url, auth, params=None, body_type=None, data=None,
                  method='GET'):
+        '''
+        :param str url: URL of API endpoint
+        :param :py:Class:'planet.auth.auth auth: Planet API authentication
+        :param dict params (opt): Request parameters
+        :param type body_type: Exptected response body type
+        :param data: Request body
+        :param str method: HTTP request method
+        '''
         self.url = url
         self.auth = auth
         self.params = params
@@ -60,6 +70,12 @@ class Response(object):
     '''Handles the Planet server's response to a HTTP request
     '''
     def __init__(self, request, http_response):
+        '''
+        :param :py:Class:`Request` request: Request that was
+            submitted to the server.
+        :param :py:Class:`requests.models.Response` http_response: Response
+            that was received from the server.
+        '''
         self.request = request
         self.http_response = http_response
         self._body = None
@@ -68,7 +84,7 @@ class Response(object):
     def body(self):
         '''The response Body
 
-        :returns Body: A Body object containing the response.
+        :returns :py:Class:`Body`: A Body object containing the response.
         '''
         if self._body is None:
             self._body = self._create_body()
@@ -117,8 +133,15 @@ class Body(object):
     '''
 
     def __init__(self, request, http_response):
+        '''
+        :param :py:Class:`planet.api.models.Request request: Request that was
+            submitted to the server.
+        :param :py:Class:'requests.models.Response http_response: Response that
+            was received from the server.
+        '''
         self._request = request
         self.response = http_response
+
         self.size = int(self.response.headers.get('content-length', 0))
         self._cancel = False
 
@@ -221,9 +244,123 @@ class Order(JSON):
         '''
         # TODO: maybe this should be like items
         # but a generator?
-        locations = iter(self.locations())
+        locations = iter(self.items())
         return locations
 
     @property
     def state(self):
         return self.get()['state']
+
+
+# class Orders(Paged):
+#     def _get_item_key(self):
+#         return 'orders'
+#
+#     def _get_links_key(self):
+#         return '_links'
+#
+#     def _get_next_key(self):
+#         return 'next'
+
+
+# def _abstract():
+#     raise NotImplementedError("You need to override this function")
+#
+#
+# class Paged(JSON):
+#     '''A paged response representing multiple results.
+#
+#     This is meant to be inherited and the following methods overridden:
+#         - _get_item_key()
+#         - _get_links_key()
+#         - _get_next_key()
+#     '''
+#     def _get_item_key(self):
+#         '''Example: return "features"'''
+#         _abstract()
+#
+#     def _get_links_key(self):
+#         '''Example: return "_links"'''
+#         _abstract()
+#
+#     def _get_next_key(self):
+#         '''Example: return "_next"'''
+#         _abstract()
+#
+#     def next(self):
+#         links = self.get()[self._get_links_key()]
+#         next_ = links.get(self._get_next_key(), None)
+#         if next_:
+#             request = Request(next_, self._request.auth, body_type=type(self))
+#             return self._dispatcher.response(request).get_body()
+#
+#     def _pages(self):
+#         page = self
+#         while page is not None:
+#             yield page
+#             page = page.next()
+#
+#     def iter(self, pages=None):
+#         '''Get an iterator of pages.
+#
+#         :param int pages: optional limit to number of pages
+#         :return: iter of this and subsequent pages
+#         '''
+#         i = self._pages()
+#         if pages is not None:
+#             i = itertools.islice(i, pages)
+#         return i
+#
+#     def items_iter(self, limit):
+#         '''Get an iterator of the 'items' in each page. Instead of a feature
+#         collection from each page, the iterator yields the features.
+#
+#         :param int limit: The number of 'items' to limit to.
+#         :return: iter of items in page
+#         '''
+#
+#         pages = (page.get() for page in self._pages())
+#         items = itertools.chain.from_iterable(
+#             (p[self._get_item_key()] for p in pages)
+#         )
+#         if limit is not None:
+#             items = itertools.islice(items, limit)
+#         return items
+#
+#     def json_encode(self, out, limit=None, sort_keys=False, indent=None):
+#         '''Encode the results of this paged response as JSON writing to the
+#         provided file-like `out` object. This function will iteratively read
+#         as many pages as present, streaming the contents out as JSON.
+#
+#         :param file-like out: an object with a `write` function
+#         :param int limit: optional maximum number of items to write
+#         :param bool sort_keys: if True, output keys sorted, default is False
+#         :param bool indent: if True, indent output, default is False
+#         '''
+#         stream = self._json_stream(limit)
+#         enc = json.JSONEncoder(indent=indent, sort_keys=sort_keys)
+#         for chunk in enc.iterencode(stream):
+#             out.write(u'%s' % chunk)
+#
+#     def _json_stream(self, limit):
+#         items = self.get()[self._get_item_key()]
+#         # if there are no results, _GeneratorAdapter doesn't play well
+#         if len(items):
+#             items = _GeneratorAdapter(self.items_iter(limit))
+#         else:
+#             items = []
+#         return {
+#             self._get_item_key(): items
+#         }
+#
+#
+# class _GeneratorAdapter(list):
+#     '''Allow a generator to be used in JSON serialization'''
+#     def __init__(self, gen):
+#         self.gen = gen
+#
+#     def __iter__(self):
+#         return self.gen
+#
+#     def __len__(self):
+#         return 1
--- tests/unit/test_models.py
+++ tests/unit/test_models.py
@@ -1,4 +1,4 @@
-# Copyright 2020 Planet Labs, Inc.
+# Copyright 2017 Planet Labs, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,9 +11,34 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import io
+import logging
+from mock import MagicMock
+
 import pytest
 
 from planet.api import exceptions, models
+
+
+LOGGER = logging.getLogger(__name__)
+
+TEST_ITEM_KEY = 'testitem'
+TEST_LINKS_KEY = 'testlinks'
+TEST_NEXT_KEY = 'testnext'
+NUM_ITEMS = 5
+
+
+@pytest.fixture
+def mocked_request():
+    return models.Request('url', 'auth')
+
+
+def mock_http_response(json, iter_content=None):
+    m = MagicMock(name='http_response')
+    m.headers = {}
+    m.json.return_value = json
+    m.iter_content = iter_content
+    return m
 
 
 def test_Request__raise_for_status():
@@ -24,3 +49,93 @@ def test_Request__raise_for_status():
 
     with pytest.raises(exceptions.OverQuota):
         models.Response._raise_for_status(429, 'exceeded QUOTA dude')
+
+
+def test_Body_write(tmpdir, mocked_request):
+    chunks = ((str(i) * 16000).encode('utf-8') for i in range(10))
+
+    body = models.Body(mocked_request, mock_http_response(
+        json=None,
+        iter_content=lambda chunk_size: chunks
+    ))
+    buf = io.BytesIO()
+    body.write(buf)
+
+    assert len(buf.getvalue()) == 160000
+
+
+# class TestPaged(models.Paged):
+#     def _get_item_key(self):
+#         return TEST_ITEM_KEY
+#
+#     def _get_links_key(self):
+#         return TEST_LINKS_KEY
+#
+#     def _get_next_key(self):
+#         return TEST_NEXT_KEY
+#
+#
+# @pytest.fixture
+# def test_paged():
+#     request = models.Request('url', 'auth')
+#
+#     # make 5 pages with 5 items on each page
+#     pages = _make_pages(5, NUM_ITEMS)
+#     http_response = mock_http_response(json=next(pages))
+#
+#     # initialize the paged object with the first page
+#     paged = TestPaged(request, http_response)
+#
+#     # the remaining 4 get used here
+#     ps = MagicMock(name='PlanetSession')
+#     ps.request.side_effect = (
+#         mock_http_response(json=p) for p in pages
+#     )
+#     # mimic dispatcher.response
+#     return paged
+#
+#
+# def _make_pages(cnt, num):
+#     '''generator of 'cnt' pages containing 'num' content'''
+#     start = 0
+#     for p in range(num):
+#         nxt = 'page %d' % (p + 1,) if p + 1 < num else None
+#         start, page = _make_test_page(cnt, start, nxt)
+#         yield page
+#
+#
+# def _make_test_page(cnt, start, nxt):
+#     '''fake paged content'''
+#     envelope = {
+#         TEST_LINKS_KEY: {
+#             TEST_NEXT_KEY: nxt
+#         },
+#         TEST_ITEM_KEY: [{
+#             'testitementry': start + t
+#         } for t in range(cnt)]
+#     }
+#     return start + cnt, envelope
+#
+
+# def test_Paged_next(test_paged):
+#     pages = list(test_paged.iter(2))
+#     assert 2 == len(pages)
+#     assert NUM_ITEMS == len(pages[0].get()[TEST_ITEM_KEY])
+#     assert NUM_ITEMS == len(pages[1].get()[TEST_ITEM_KEY])
+#
+#
+# def test_Paged_iter(test_paged):
+#     pages = list(test_paged.iter(2))
+#     assert 2 == len(pages)
+#     assert NUM_ITEMS == len(pages[0].get()[TEST_ITEM_KEY])
+#     assert NUM_ITEMS == len(pages[1].get()[TEST_ITEM_KEY])
+#
+#
+# @pytest.mark.skip(reason='not implemented')
+# def test_Paged_items_iter():
+#     pass
+#
+#
+# @pytest.mark.skip(reason='not implemented')
+# def test_Paged_json_encode():
+#     pass
