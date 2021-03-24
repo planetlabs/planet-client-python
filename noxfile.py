1e2a8daeccbc212d91f1da92ac2133e94578f7db
jreiberkyle <jreiberkyle@users.noreply.github.com>
2021-03-23 17:12:02 -0700

    tox -> nox

---
+++ noxfile.py
@@ -0,0 +1,24 @@
+import nox
+
+nox.options.stop_on_first_error = True
+nox.options.reuse_existing_virtualenvs = True
+
+source_files = ("planet", "tests", "setup.py", "noxfile.py")
+
+
+@nox.session(python=["3.7", "3.8", "3.9"])
+def test(session):
+    session.install("-e", ".[test]")
+
+    options = session.posargs
+    if "-k" or "-x" in options:
+        options.append("--no-cov")
+
+    session.run("pytest", "-v", *options)
+
+
+@nox.session
+def lint(session):
+    session.install("-e", ".[dev]")
+
+    session.run("flake8", *source_files)
