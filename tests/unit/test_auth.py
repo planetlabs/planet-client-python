d47d54b60600d85ea78cd08967e7cc7d7e20eb7c
jreiberkyle <jreiberkyle@users.noreply.github.com>
2021-05-18 17:25:44 -0700

    fill in auth test coverage

--- tests/unit/test_auth.py
+++ tests/unit/test_auth.py
@@ -25,9 +25,23 @@ from planet import auth
 LOGGER = logging.getLogger(__name__)
 
 
+def test_Auth_from_key():
+    test_auth_env1 = auth.Auth.from_key('testkey')
+    assert test_auth_env1.value == 'testkey'
+
+
 def test_Auth_from_key_empty():
     with pytest.raises(auth.APIKeyAuthException):
         _ = auth.Auth.from_key('')
+
+
+# def test_Auth_from_file(tmp_path):
+#     secret_path = str(tmp_path / '.test')
+#     with open(secret_path, 'w') as fp:
+#         fp.write('{"key": "testvar"}')
+#
+#     test_auth = auth.Auth.from_file(secret_path)
+#     assert test_auth.value == 'testvar'
 
 
 def test_Auth_from_file_alternate_success(tmp_path):
@@ -54,13 +68,25 @@ def test_Auth_from_file_alternate_wrongformat(tmp_path):
         _ = auth.Auth.from_file(secret_path)
 
 
+def test_Auth_from_env(monkeypatch):
+    monkeypatch.setenv('PL_API_KEY', 'testkey')
+    test_auth_env = auth.Auth.from_env()
+    assert test_auth_env.value == 'testkey'
+
+
+def test_Auth_from_env_failure(monkeypatch):
+    monkeypatch.delenv('PL_API_KEY', raising=False)
+    with pytest.raises(auth.AuthException):
+        _ = auth.Auth.from_env()
+
+
 def test_Auth_from_env_alternate_success(monkeypatch):
     alternate = 'OTHER_VAR'
     monkeypatch.setenv(alternate, 'testkey')
     monkeypatch.delenv('PL_API_KEY', raising=False)
 
-    test_auth_env1 = auth.Auth.from_env(alternate)
-    assert test_auth_env1.value == 'testkey'
+    test_auth_env = auth.Auth.from_env(alternate)
+    assert test_auth_env.value == 'testkey'
 
 
 def test_Auth_from_env_alternate_doesnotexist(monkeypatch):
