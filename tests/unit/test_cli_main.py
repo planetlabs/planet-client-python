7755a31680ea64563d7980eb0a0707425187cf1c
Mansi Shah <mkshah605@gmail.com>
2022-03-10 10:33:49 -0500

    updated verbosity handling in cli.py to use strings instead of v's. Incorporated new tests to make sure they are printing to stderr.

--- tests/unit/test_cli_main.py
+++ tests/unit/test_cli_main.py
@@ -18,6 +18,8 @@ from click.testing import CliRunner
 
 from planet.cli import cli
 
+import pytest
+
 LOGGER = logging.getLogger(__name__)
 
 
@@ -36,7 +38,28 @@ def test_cli_info_verbosity(monkeypatch):
         log_level = level
     monkeypatch.setattr(cli.logging, 'basicConfig', configtest)
 
+def test_loggingoutput_normal(capfd): 
+    # test a normal case
     runner = CliRunner()
-    result = runner.invoke(cli.main, args=['test'])
-    assert result.exit_code == 0
-    assert log_level == logging.WARNING
+    result = runner.invoke(cli.main, args=['--verbosity', 'debug', 'orders'], catch_exceptions=True)
+    assert "Commands for interacting with the Orders API" in result.output
+
+def test_loggingoutput_space(capfd): 
+    # test a case with extra spaces (should still work)
+    runner = CliRunner()
+    result = runner.invoke(cli.main, args=['--verbosity', ' debug ', 'orders'], catch_exceptions=True)
+    assert "Commands for interacting with the Orders API" in result.output
+
+
+def test_loggingoutput_misspell(capfd): 
+    # test a case where the argument is misspelled
+    runner = CliRunner()
+    result = runner.invoke(cli.main, args=['--verbosity', 'debu', 'orders'], catch_exceptions=True)
+    assert "please set verbosity" in str(result.exception)
+
+
+def test_loggingoutput_number(capfd): 
+    # test a case when input includes number instead of string
+    runner = CliRunner()
+    result = runner.invoke(cli.main, args=['--verbosity', 45, 'orders'], catch_exceptions=True)
+    assert "please set verbosity" in str(result.exception)
