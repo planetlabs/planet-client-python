ee9eceb9b09ce0cea72097cd61595801bffde7a4
Carl A. Adams <carl.adams@planet.com>
2022-04-11 15:54:23 -0700

    Remove go pattern make rules

--- build-tools/make/go-defs.mk
+++
@@ -1,3 +0,0 @@
-GO_EXE:=$(if $(strip $(GO_EXE)),$(strip $(GO_EXE)),go)
-GOLINT_EXE:=$(if $(strip $(LINT_EXE)),$(strip $(GOLINT_EXE)),golangci-lint)
-GO_PROJECT_SRC:=$(if $(strip $(GO_PROJECT_SRC)),$(strip $(GO_PROJECT_SRC)),src)
