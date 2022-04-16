4105d625d0042b4c364bea07afb28c88ad7286b1
Carl A. Adams <carl.adams@planet.com>
2022-04-15 20:30:51 -0700

    externalize constants.  Rename py file.

--- planet/cli/constants.py
+++ planet/cli/constants.py
@@ -1,8 +1,16 @@
 from planet.auth.oidc.auth_clients.auth_code_flow import AuthCodePKCEClientConfig
 
 
-# The well known OIDC client that is the Planet Python CLI
+ENV_AUTH_CLIENT_CONFIG_FILE = 'PL_AUTH_CLIENT_CONFIG_FILE'
+ENV_AUTH_PROFILE = 'PL_AUTH_PROFILE'
+ENV_AUTH_SCOPES = 'PL_AUTH_SCOPES'
+ENV_AUTH_TOKEN_FILE = 'PL_AUTH_TOKEN_FILE'
+ENV_FOO_ID = 'PL_FOO_ID'
+ENV_FOO_SERVICE_URL = 'PL_FOO_SERVICE_URL'
+ENV_LOGLEVEL = 'PL_LOGLEVEL'
+
 DEFAULT_OIDC_CLIENT_CONFIG = AuthCodePKCEClientConfig(
+    # The well known OIDC client that is the Planet Python CLI
     # FIXME: a better URL for production
     auth_server='https://account.planet.com/oauth2/aus2enhwueFYRb50S4x7',
     client_id='0oa2scq915nekGLum4x7',
--- planet/cli/options.py
+++ planet/cli/options.py
@@ -2,6 +2,16 @@ import click
 import pathlib
 
 
+from planet.cx.commands.cli.constants import \
+    ENV_AUTH_CLIENT_CONFIG_FILE, \
+    ENV_AUTH_PROFILE, \
+    ENV_AUTH_SCOPES, \
+    ENV_AUTH_TOKEN_FILE, \
+    ENV_FOO_ID, \
+    ENV_FOO_SERVICE_URL, \
+    ENV_LOGLEVEL
+
+
 # TODO: rename simply profile, since it could be used for non-auth reasons?
 def opt_auth_profile(function):
     function = click.option(
@@ -12,8 +22,8 @@ def opt_auth_profile(function):
         #       package or the like.
         # type=click.Choice(...),
         type=str,
-        envvar='PL_AUTH_PROFILE',
-        help='Select the OIDC client profile to use.\nEnvironment variable: PL_AUTH_PROFILE',
+        envvar=ENV_AUTH_PROFILE,
+        help='Select the client profile to use.\nEnvironment variable: ' + ENV_AUTH_PROFILE,
         default='',  # 'default', # just construct our default paths inside ~/.planet, not ~/.planet/<profile>
         show_default=True,
         is_eager=True)(function)
@@ -24,9 +34,9 @@ def opt_auth_client_config_file(function):
     function = click.option(
         '--auth-client-config-file',
         type=click.Path(),
-        envvar='PL_AUTH_CLIENT_CONFIG_FILE',
+        envvar=ENV_AUTH_CLIENT_CONFIG_FILE,
         help='Auth client configuration file. The default will be constructed to '
-             '~/.planet/<auth_profile>/auth_client.json\nEnvironment variable: PL_AUTH_CLIENT_CONFIG_FILE',
+             '~/.planet/<auth_profile>/auth_client.json\nEnvironment variable: ' + ENV_AUTH_CLIENT_CONFIG_FILE,
         default=None,
         show_default=True,
         callback=lambda ctx, param, value: pathlib.Path(value) if value else pathlib.Path.home().joinpath(".planet/{}/auth_client.json".format(ctx.params['auth_profile'])))(function) # noqa
@@ -36,7 +46,7 @@ def opt_auth_client_config_file(function):
 def opt_foo_id_required(function):
     function = click.option(
         '--foo-id',
-        type=str, envvar='PL_FOO_ID',
+        type=str, envvar=ENV_FOO_ID,
         help='Specify the id of a foo.',
         required=True)(function)
     return function
@@ -46,9 +56,9 @@ def opt_foo_service_url(function):
     function = click.option(
         '--foo-service-url',
         type=str,
-        envvar='PL_FOO_SERVICE_URL',
+        envvar=ENV_FOO_SERVICE_URL,
         help='Specify the URL for the foo service endpoint.'
-             '\nEnvironment variable: PL_FOO_SERVICE_URL',
+             '\nEnvironment variable: ' + ENV_FOO_SERVICE_URL,
         default='http://localhost:8081',
         show_default=True)(function)
     return function
@@ -57,8 +67,8 @@ def opt_foo_service_url(function):
 def opt_loglevel(function):
     function = click.option(
         '-l', '--loglevel',
-        envvar='PL_LOGLEVEL',
-        help='Set the log level.\nEnvironment variable: PL_LOGLEVEL',
+        envvar=ENV_LOGLEVEL,
+        help='Set the log level.\nEnvironment variable: ' + ENV_LOGLEVEL,
         type=click.Choice(['CRITICAL', 'ERROR', 'WARNING', 'INFO', 'DEBUG'], case_sensitive=False),
         default='INFO',
         show_default=True)(function)
@@ -78,9 +88,9 @@ def opt_token_file(function):
     function = click.option(
         '--token-file',
         type=click.Path(),
-        envvar='PL_TOKEN_FILE',
+        envvar=ENV_AUTH_TOKEN_FILE,
         help='Auth token file. The default will be constructed to '
-             '~/.planet/<auth_profile>/token.json\nEnvironment variable: PL_TOKEN_FILE',
+             '~/.planet/<auth_profile>/token.json\nEnvironment variable: ' + ENV_AUTH_TOKEN_FILE,
         default=None,
         show_default=True,
         callback=lambda ctx, param, value: pathlib.Path(value) if value else pathlib.Path.home().joinpath(".planet/{}/token.json".format(ctx.params['auth_profile'])))(function) # noqa
@@ -92,10 +102,10 @@ def opt_token_scope(function):
         '--scope',
         multiple=True,
         type=str,
-        envvar='PL_AUTH_SCOPES',
+        envvar=ENV_AUTH_SCOPES,
         help='Token scopes to request. Specify multiple options to request multiple scopes. '
              'When set via environment variable, scopes should be white space delimited. '
-             '\nEnvironment variable: PL_AUTH_SCOPES',
+             '\nEnvironment variable: ' + ENV_AUTH_SCOPES,
         default=None,
         show_default=True)(function)
     return function
