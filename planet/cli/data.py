82e6c7a548f06bc8f197274614bd5d73d33714d2
Sean Gillies <sean.gillies@gmail.com>
2022-04-28 14:19:50 -0600

    Add a first Data CLI test and module stub (#449)
    * Add a first Data CLI test and module stub
    
    Resolves #433
    
    * Moar comments
    
    * Formatting
    
    * Add unit tests for Data CLI parameter callbacks
    
    And use echo_json and translate_errors.
    
    * Fix example usage
    
    * Eliminate an if/else
    
    * Remove special case from parse_item_types

---
+++ planet/cli/data.py
@@ -0,0 +1,114 @@
+"""The Planet Data CLI."""
+
+import json
+from typing import AsyncIterator, List
+
+import click
+
+from .cmds import coro, translate_exceptions
+from .io import echo_json
+
+# Parameter callbacks are what we use to validate and transform the
+# values of arguments and options. On the command line, everything is
+# a string, but we want to transform to natural Python types as soon as
+# possible.
+
+
+def parse_item_types(ctx, param, value: str) -> List[str]:
+    """Turn a string of comma-separated names into a list of names."""
+    # Note: we could also normalize case and validate the names against
+    # our schema here.
+    return [part.strip() for part in value.split(",")]
+
+
+def parse_filter(ctx, param, value: str) -> dict:
+    """Turn filter JSON into a dict."""
+    return json.loads(value)
+
+
+@click.group()
+@click.pass_context
+def data(ctx):
+    """A group of commands for interacting with the Data API."""
+
+
+# TODO: filter().
+
+
+@data.command()
+@translate_exceptions
+# Our CLI command functions are async functions. They need an event
+# loop to be executed. The coro decorator provides that event loop.
+@coro
+# The command has two positional arguments. ITEM_TYPES comes first.
+# By default these are required and will evaluate to str type.
+@click.argument("item_types", callback=parse_item_types)
+@click.argument("filter", callback=parse_filter)
+# The command has three options.
+@click.option("--limit",
+              type=int,
+              default=100,
+              help="Maximum number of results to return.")
+@click.option("--name", help="Name for the saved search.")
+@click.option("--pretty", is_flag=True, help="Pretty print output.")
+# This decorator gives our function a context (named "ctx") that may
+# contain parameters set by the "planet data" command group, like
+# "planet --quiet data" or "planet --verbosity=debug data".
+@click.pass_context
+async def search_quick(ctx,
+                       item_types: List[str],
+                       filter: dict,
+                       limit: int,
+                       pretty: bool,
+                       name: str):
+    """Execute a structured item search and print results.
+
+    Results are represented as a sequence of GeoJSON features.
+
+    """
+
+    # Note: API tokens will be found in "ctx". They are unused in this
+    # example.
+    #
+    # The Planet Data client will return a stream of GeoJSON
+    # Feature-like dicts (aka an "iterator"). The search_quick function
+    # will iterate over the features, encode them as JSON, and echo
+    # them. We simulate that here.
+    async def example_client(item_types,
+                             filter,
+                             limit=0,
+                             name=None) -> AsyncIterator[dict]:
+        """This is a placeholder.
+
+        We'll delete this and use the real method from
+        planet.clients.data when it is available.
+
+        """
+        # Note: we re-emit the input item types and filter to help us
+        # sanity check during early sprints.
+        for feature in item_types:
+            yield dict(item_type=feature)
+
+        yield dict(filter=filter)
+
+    # CLI functions raise ClickException when any kind of intentionally
+    # raised PlanetError occurs. We *don't* handle other errors now
+    # because those will be caused by bugs in our code and we want them
+    # raw and unfiltered.
+    async for feature in example_client(item_types,
+                                        filter,
+                                        limit=limit,
+                                        name=name):
+        echo_json(feature, pretty=pretty)
+
+
+# TODO: search_create()".
+# TODO: search_update()".
+# TODO: search_delete()".
+# TODO: search_run()".
+# TODO: item_get()".
+# TODO: asset_activate()".
+# TODO: asset_wait()".
+# TODO: asset_download()".
+# TODO: search_create()".
+# TODO: stats()".
---
+++ tests/integration/test_data_cli.py
@@ -0,0 +1,55 @@
+"""Tests of the Data CLI."""
+
+from click.testing import CliRunner
+import pytest
+
+from planet.cli import cli
+
+
+def test_data_command_registered():
+    """planet-data command prints help and usage message."""
+    result = CliRunner().invoke(cli.main, ["data", "--help"])
+    assert result.exit_code == 0
+    assert "Usage" in result.output
+    assert "search-quick" in result.output
+    # Add other sub-commands here.
+
+
+# TODO: basic test for "planet data filter".
+
+
+# We expect this test to fail now. When the Data API client is
+# available, we will remove the xfail marker and work to get this test,
+# or a better version, to pass.
+@pytest.mark.xfail(reason="Data client not yet implemented")
+def test_search_quick():
+    """planet data search-quick prints 1 GeoJSON Feature."""
+    result = CliRunner().invoke(
+        cli.main,
+        # When testing, we "explode" our command and its parameters
+        # into a list to make parameterization more clear.
+        [
+            "data",
+            "search-quick",
+            # To keep yapf from putting option name and value on
+            # different lines, use a "=".
+            "--limit=10",
+            "--name=test",
+            "--pretty",
+            "lol,wut",
+            "{}"
+        ])
+    assert result.exit_code == 0
+    assert "Feature" in result.output
+
+
+# TODO: basic test for "planet data search-create".
+# TODO: basic test for "planet data search-update".
+# TODO: basic test for "planet data search-delete".
+# TODO: basic test for "planet data search-run".
+# TODO: basic test for "planet data item-get".
+# TODO: basic test for "planet data asset-activate".
+# TODO: basic test for "planet data asset-wait".
+# TODO: basic test for "planet data asset-download".
+# TODO: basic test for "planet data search-create".
+# TODO: basic test for "planet data stats".
