4460b1dd8be9df3c07f9fc6587b5e607d7896f08
Carl A. Adams <carl.adams@planet.com>
2022-05-17 09:46:43 -0700

    the coverage games

--- tests/integration/test_auth_cli.py
+++ tests/integration/test_auth_cli.py
@@ -14,7 +14,7 @@
 
 import unittest
 
-# import pytest
+import pytest
 from click.testing import CliRunner
 
 from planet.cli import cli
@@ -50,56 +50,65 @@ class AuthTest(unittest.TestCase):
         result = self.invoke_auth_cli(['login'])
         self.assertEqual(0, result.exit_code)
 
-    # @pytest.mark.skip(
-    #     'Skipping test that is specific to a particular auth implementation')
-    # def test_print_access_token(self):
-    #     result = self.invoke_auth_cli(['print-access-token'])
-    #     self.assertEqual(0, result.exit_code)
+    @pytest.mark.xfail(
+        reason='Tests specific to a particular auth implementation expected '
+        'to fail at this time')
+    def test_print_access_token(self):
+        result = self.invoke_auth_cli(['print-access-token'])
+        self.assertEqual(0, result.exit_code)
 
-    # @pytest.mark.skip(
-    #     'Skipping test that is specific to a particular auth implementation')
-    # def test_print_api_key(self):
-    #     result = self.invoke_auth_cli(['print-api-key'])
-    #     self.assertEqual(0, result.exit_code)
+    @pytest.mark.xfail(
+        reason='Tests specific to a particular auth implementation expected '
+        'to fail at this time')
+    def test_print_api_key(self):
+        result = self.invoke_auth_cli(['print-api-key'])
+        self.assertEqual(0, result.exit_code)
 
-    # @pytest.mark.skip(
-    #     'Skipping test that is specific to a particular auth implementation')
-    # def test_refresh(self):
-    #     result = self.invoke_auth_cli(['refresh'])
-    #     self.assertEqual(0, result.exit_code)
+    @pytest.mark.xfail(
+        reason='Tests specific to a particular auth implementation expected '
+        'to fail at this time')
+    def test_refresh(self):
+        result = self.invoke_auth_cli(['refresh'])
+        self.assertEqual(0, result.exit_code)
 
-    # @pytest.mark.skip(
-    #     'Skipping test that is specific to a particular auth implementation')
-    # def test_validate_access_token(self):
-    #     result = self.invoke_auth_cli(['validate-access-token'])
-    #     self.assertEqual(0, result.exit_code)
+    @pytest.mark.xfail(
+        reason='Tests specific to a particular auth implementation expected '
+        'to fail at this time')
+    def test_validate_access_token(self):
+        result = self.invoke_auth_cli(['validate-access-token'])
+        self.assertEqual(0, result.exit_code)
 
-    # @pytest.mark.skip(
-    #     'Skipping test that is specific to a particular auth implementation')
-    # def test_validate_id_token(self):
-    #     result = self.invoke_auth_cli(['validate-id-token'])
-    #     self.assertEqual(0, result.exit_code)
+    @pytest.mark.xfail(
+        reason='Tests specific to a particular auth implementation expected '
+        'to fail at this time')
+    def test_validate_id_token(self):
+        result = self.invoke_auth_cli(['validate-id-token'])
+        self.assertEqual(0, result.exit_code)
 
-    # @pytest.mark.skip(
-    #     'Skipping test that is specific to a particular auth implementation')
-    # def test_validate_id_token_local(self):
-    #     result = self.invoke_auth_cli(['validate-id-token-local'])
-    #     self.assertEqual(0, result.exit_code)
+    @pytest.mark.xfail(
+        reason='Tests specific to a particular auth implementation expected '
+        'to fail at this time')
+    def test_validate_id_token_local(self):
+        result = self.invoke_auth_cli(['validate-id-token-local'])
+        self.assertEqual(0, result.exit_code)
 
-    # @pytest.mark.skip(
-    #     'Skipping test that is specific to a particular auth implementation')
-    # def test_validate_refresh_token(self):
-    #     result = self.invoke_auth_cli(['validate-refresh-token'])
-    #     self.assertEqual(0, result.exit_code)
+    @pytest.mark.xfail(
+        reason='Tests specific to a particular auth implementation expected '
+        'to fail at this time')
+    def test_validate_refresh_token(self):
+        result = self.invoke_auth_cli(['validate-refresh-token'])
+        self.assertEqual(0, result.exit_code)
 
-    # @pytest.mark.skip(
-    #     'Skipping test that is specific to a particular auth implementation')
-    # def test_revoke_access_token(self):
-    #     result = self.invoke_auth_cli(['revoke-access-token'])
-    #     self.assertEqual(0, result.exit_code)
+    @pytest.mark.xfail(
+        reason='Tests specific to a particular auth implementation expected '
+        'to fail at this time')
+    def test_revoke_access_token(self):
+        result = self.invoke_auth_cli(['revoke-access-token'])
+        self.assertEqual(0, result.exit_code)
 
-    # @pytest.mark.skip(
-    #     'Skipping test that is specific to a particular auth implementation')
-    # def test_revoke_refresh_token(self):
-    #     result = self.invoke_auth_cli(['revoke-refresh-token'])
-    #     self.assertEqual(0, result.exit_code)
+    @pytest.mark.xfail(
+        reason='Tests specific to a particular auth implementation expected '
+        'to fail at this time')
+    def test_revoke_refresh_token(self):
+        result = self.invoke_auth_cli(['revoke-refresh-token'])
+        self.assertEqual(0, result.exit_code)
