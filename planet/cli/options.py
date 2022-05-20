9e931575a75ea5619f9b4a91fb387ff223962851
Carl A. Adams <carl.adams@planet.com>
2022-05-20 15:41:20 -0700

    upstream

--- planet/cli/options.py
+++ planet/cli/options.py
@@ -8,6 +8,7 @@ from planet.auth.constants import \
     ENV_AUTH_SCOPES, \
     ENV_AUTH_TOKEN_FILE, \
     ENV_AUTH_USERNAME, \
+    TOKEN_FILE_SOPS, \
     TOKEN_FILE_PLAIN, \
     AUTH_CONFIG_FILE_SOPS, \
     AUTH_CONFIG_FILE_PLAIN
@@ -122,6 +123,14 @@ def opt_loglevel(function):
     return function
 
 
+def opt_okta_tenant(function):
+    function = click.option('--okta-tenant', type=str, envvar='PL_OKTA_TENANT',
+                            help='Specify the URL of the okta tenant.'
+                            '\nEnvironment variable: PL_OKTA_TENANT',
+                            default='https://account-next.planet.com', show_default=True)(function)
+    return function
+
+
 def opt_open_browser(function):
     function = click.option(
         '--open-browser/--no-open-browser',
@@ -136,15 +145,18 @@ def opt_token_file(function):
         '--token-file',
         type=click.Path(),
         envvar=ENV_AUTH_TOKEN_FILE,
-        help='Auth token file. The default will be constructed to '
-        '~/.planet/<auth_profile>/' + TOKEN_FILE_PLAIN,
+        help='Auth token file. The default will be constructed to'
+        ' ~/.planet/<auth_profile>/{} or ~/.planet/<auth_profile>/{}'.format(
+            TOKEN_FILE_SOPS, TOKEN_FILE_PLAIN),
         default=None,
         show_envvar=True,
         show_default=True,
         callback=lambda ctx,
         param,
-        value: Profile.get_profile_file_path(
-            TOKEN_FILE_PLAIN, ctx.params['auth_profile'], value))(function)
+        value: Profile.get_profile_file_path_with_priority(
+            [TOKEN_FILE_SOPS, TOKEN_FILE_PLAIN],
+            ctx.params['auth_profile'],
+            value))(function)
     return function
 
 
