02f48607a37740361f4280e30a62b32698940049
Kevin Lacaille <kevinlacaille@gmail.com>
2022-08-10 08:45:56 -0400

    Undo extra spaces.

--- planet/cli/types.py
+++ planet/cli/types.py
@@ -29,12 +29,16 @@ class CommaSeparatedString(click.types.StringParamType):
             convlist = value
         else:
             convstr = super().convert(value, param, ctx)
+
             if convstr == '':
                 self.fail('Entry cannot be an empty string.')
+
             convlist = [part.strip() for part in convstr.split(",")]
+
             for v in convlist:
                 if not v:
                     self.fail(f'Empty entry encountered in "{value}".')
+
         return convlist
 
 
