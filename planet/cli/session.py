791af372ddd56595e871f4b1b84a285a02cab2ff
Jennifer Reiber Kyle <jreiberkyle@users.noreply.github.com>
2022-12-01 20:23:09 -0800

    yapf fix

--- planet/cli/session.py
+++ planet/cli/session.py
@@ -6,6 +6,7 @@ from planet.http import Session
 
 class CliSession(Session):
     """Session with CLI-specific auth and identifying header"""
+
     def __init__(self):
         super().__init__(Auth.from_file())
         self._client.headers.update({'X-Planet-App': 'python-cli'})
