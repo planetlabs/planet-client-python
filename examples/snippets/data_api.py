a33d17fb8f3be3ebf5816f2477dcf40eeb866638
Kevin Lacaille <kevinlacaille@gmail.com>
2023-04-25 15:50:02 -0700

    Created skeleton for data API snippets.

---
+++ examples/snippets/data_api.py
@@ -0,0 +1,68 @@
+# Copyright 2023 Planet Labs PBC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy of
+# the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under
+# the License.
+"""Example of creating and downloading multiple orders.
+
+This is an example of submitting two orders, waiting for them to complete, and
+downloading them. The orders each clip a set of images to a specific area of
+interest (AOI), so they cannot be combined into one order.
+
+[Planet Explorer](https://www.planet.com/explorer/) was used to define
+the AOIs and get the image ids.
+"""
+import json
+from pathlib import Path
+import planet
+
+
+# search
+
+
+# create_search
+
+
+# update_search
+
+
+# list_searches
+
+
+# delete_search
+
+
+# get_search
+
+
+# run_search
+
+
+# get_stats
+
+
+# list_item_assets
+
+
+# get_asset
+
+
+# activate_asset
+
+
+# wait_asset
+
+
+# download_asset w/o checksum
+
+
+# download_asset w checksum
+
