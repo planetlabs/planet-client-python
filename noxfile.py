838ff7c7d9332deda50337b83b1516e1cc65ade0
Sean Gillies <sean.gillies@gmail.com>
2023-07-03 20:22:09 -0600

    Add a 3.12 nox session

--- noxfile.py
+++ noxfile.py
@@ -37,7 +37,7 @@ def coverage(session):
     session.run('coverage', 'report')
 
 
-@nox.session(python=["3.7", "3.8", "3.9", "3.10", "3.11"])
+@nox.session(python=["3.8", "3.9", "3.10", "3.11", "3.12"])
 def test(session):
     session.install(".[test]")
 
