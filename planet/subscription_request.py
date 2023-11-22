f35bb0e9399ac532c5dbcddef0bf7277cfe4df51
gnhmu <stefan.jozefowicz@bayer.com>
2023-11-22 21:10:40 +0100

    fix timedelta 0 evaluation leading to malformed isoformat dt strings

--- planet/subscription_request.py
+++ planet/subscription_request.py
@@ -358,7 +358,7 @@ def planetary_variable_source(
 def _datetime_to_rfc3339(value: datetime) -> str:
     """Converts the datetime to an RFC3339 string"""
     iso = value.isoformat()
-    if not value.utcoffset():
+    if value.utcoffset() is None:
         # rfc3339 needs a Z if there is no timezone offset
         iso += 'Z'
     return iso
