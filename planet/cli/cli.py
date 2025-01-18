324a1c4ab0e9615c72f9bc737152ffb4c1bd01cb
Carl A. Adams <carl.adams@planet.com>
2025-01-18 09:18:51 -0800

    harmonize flak8 and yapf

--- planet/cli/cli.py
+++ planet/cli/cli.py
@@ -75,8 +75,7 @@ def _configure_cli_auth_ctx(ctx,
             auth_profile_opt=auth_profile,
             auth_client_id_opt=auth_client_id,
             auth_client_secret_opt=auth_client_secret,
-            auth_api_key_opt=auth_api_key,
-        )
+            auth_api_key_opt=auth_api_key)
 
     # planet SDK Auth context type
     ctx.obj['PLSDK_AUTH'] = planet.Auth.from_plauth(
