d8b9c913d347912ed6d293d52be20b4f99164087
Carl A. Adams <carl.adams@planet.com>
2025-06-05 14:23:42 -0700

    minor edits to example code

--- examples/auth-session-management/cli_managed_auth_state__specific_auth_profile.py
+++ examples/auth-session-management/cli_managed_auth_state__specific_auth_profile.py
@@ -11,7 +11,7 @@ def example_main():
     # If this has not been done, the API calls will fail.  This example
     # does not initialize a new session.
     plsdk_auth = planet.Auth.from_profile(
-        profile_name="my-cli-managed-profile")
+        profile_name="my-app-profile")
 
     # If required, how to login depends on what is configured in the specific
     # profile.  See other examples for login calls.
